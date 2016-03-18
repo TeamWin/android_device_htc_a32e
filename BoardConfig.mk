1953b43d2380b08facb98d38d3fa74319dc45826
Captain Throwback <captainthrowback@hotmail.com>
2016-03-18 12:42:07 -0400

    Update prebuilt kernel to support new firmware
    - Clean up BoardConfig
    - Remove USB-OTG as unsupported
    
    Change-Id: I91d166083e17fb75cd70319e0596cf0d225a221d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,36 +15,34 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a7
 
+# Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk vmalloc=400m androidboot.hardware=htc_a32e androidusb.pid=0x0668 androidboot.mode=normal
-
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x80008000 --ramdisk_offset 0x82000000 --tags_offset 0x81e00000 --dt device/htc/a32e/dt.img
-
-# prebuilt kernel
 TARGET_PREBUILT_KERNEL := device/htc/a32e/kernel
-# else uncomment below to build from sauce
-# TARGET_KERNEL_SOURCE := kernel/htc/a32e
-# TARGET_KERNEL_CONFIG := msm8916_defconfig
 
+# Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 25165824
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 25165824
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-#TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 
-TW_THEME := portrait_hdpi
-#RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-#TW_INCLUDE_CRYPTO := true
-TW_NO_EXFAT_FUSE := true
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_SECURE_ERASE := true
+
+# TWRP Build Flags
+TW_THEME := portrait_hdpi
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_HAS_DOWNLOAD_MODE := true
+#TW_INCLUDE_CRYPTO := true
+#TW_NO_EXFAT_FUSE := true
+#TWRP_EVENT_LOGGING := true
 
 # Vendor Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
