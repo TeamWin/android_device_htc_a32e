99d873100a9eaa195276a12d3ec157c0554527c2
Captain Throwback <captainthrowback@hotmail.com>
2017-02-17 15:06:05 -0500

    a32e: Update files for 6.0
    include stock decryption support - requires:
    https://gerrit.omnirom.org/#/c/21050/
    
    Change-Id: I403be787a6438036452e2657e63e39443a489b8d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,10 +16,10 @@ TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a7
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk vmalloc=400m androidboot.hardware=htc_a32e androidusb.pid=0x0668 androidboot.mode=normal
-BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk vmalloc=400m androidboot.hardware=htc_a32e androidkey.dummy=1 androidusb.pid=0x066A disk_mode_enable=1 androidtouch.htc_event=1 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x80008000 --ramdisk_offset 0x82000000 --tags_offset 0x81e00000 --dt device/htc/a32e/dt.img
+BOARD_MKBOOTIMG_ARGS := --board recovery:0 --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --dt device/htc/a32e/dt.img
 TARGET_PREBUILT_KERNEL := device/htc/a32e/kernel
 
 # Partitions
@@ -30,20 +30,24 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
+#TARGET_USERIMAGES_USE_F2FS := true
 
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_SECURE_ERASE := true
+#TARGET_USES_LOGD := true
 
 # TWRP Build Flags
 TW_THEME := portrait_hdpi
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_HAS_DOWNLOAD_MODE := true
 TARGET_RECOVERY_DEVICE_MODULES := chargeled
-#TW_INCLUDE_CRYPTO := true
-#TW_NO_EXFAT_FUSE := true
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
+TW_NO_EXFAT_FUSE := true
 #TWRP_EVENT_LOGGING := true
+#TWRP_INCLUDE_LOGCAT := true
 
 # Vendor Init
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_a32e
